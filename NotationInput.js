c091ab89a9d06fc7dba1fdc27382ba52a3ab8cf7
tarmo <trmjhnns@gmail.com>
2023-05-10 10:57:12 +0300

    Better placement for Switch.

--- NotationInput.js
+++ NotationInput.js
@@ -757,9 +757,9 @@ export function NotationInput({lyStart, setNotationInfo, notationInfo, selectedN
 
     return <div className={"h5p-musical-dictations-uiDiv h5peditor-notationInput-margin-top"} >
         <Grid container direction={"column"} spacing={1}>
-            { !showTimeAndClefInput && showLilypond && // do not show the control in editor - then no choice, showLilypond should be true and the textarea always shown
+            { !showTimeAndClefInput  && // do not show the control in editor - then no choice, showLilypond should be true and the textarea always shown
                 <FormGroup>
-                    <FormControlLabel control={<Switch size={"small"} checked={showLilypond}
+                    <FormControlLabel control={<Switch sx={{marginLeft: 1.1}} size={"small"} checked={showLilypond}
                                                        onChange={() => {
                                                            setShowLilypond(!showLilypond);
                                                            if (resizeFunction) resizeFunction();
